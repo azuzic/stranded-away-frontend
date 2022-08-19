d25449517010ab1ffdadb2fb93a8d9a57eeb6c10
lukablaskovic <lblaskovi@unipu.hr>
2022-08-19 13:17:53 +0200

    moved timeline editor to componenet

--- src/views/AdminPanelView.vue
+++ src/views/AdminPanelView.vue
@@ -139,205 +139,16 @@
         <!--***TIMELINE EDITOR***-->
         <v-col>
           <v-container v-if="currentPanelItem == 1">
-            <v-card class="mx-auto" max-width="500">
-              <v-card-title
-                class="text-h6 font-weight-regular justify-space-between"
-              >
-                <span>{{ currentTitle }}</span>
-                <v-avatar
-                  color="error"
-                  class="subheading white--text"
-                  size="24"
-                  v-text="step"
-                ></v-avatar>
-              </v-card-title>
-              <validation-observer ref="observer" v-slot="{ invalid }">
-                <form class="" @submit.prevent="addNewTimelinePost">
-                  <v-window v-model="step">
-                    <!--PAGE 1-->
-                    <v-window-item :value="1">
-                      <v-card-text>
-                        <!--Title-->
-                        <validation-provider
-                          v-slot="{ errors }"
-                          name="Title"
-                          rules="required|max:50|min:3"
-                        >
-                          <v-text-field
-                            v-model="timelinePost.title"
-                            :counter="50"
-                            label="Title"
-                            :error-messages="errors"
-                            required
-                          ></v-text-field>
-                        </validation-provider>
-                        <!--/Title-->
-
-                        <!--Text-->
-                        <validation-provider
-                          v-slot="{ errors }"
-                          name="Text"
-                          rules="required"
-                        >
-                          <v-textarea
-                            class="mt-2 mb-2"
-                            clearable
-                            filled
-                            :counter="2000"
-                            auto-grow
-                            label="Text"
-                            v-model="timelinePost.text"
-                            required
-                            :error-messages="errors"
-                          ></v-textarea>
-                        </validation-provider>
-                        <!--/Text-->
-                      </v-card-text>
-                    </v-window-item>
-                    <!--/PAGE 1-->
-                    <!--Icon-->
-                    <v-window-item :value="2">
-                      <v-card-text>
-                        <validation-provider v-slot="{ errors }" name="Icon">
-                          <v-select
-                            v-model="selectedIcon"
-                            :items="availableIcons"
-                            label="Icon"
-                            prepend-icon="mdi-robot"
-                            :error-messages="errors"
-                            data-vv-name="selectedIcon"
-                          >
-                          </v-select>
-                        </validation-provider>
-
-                        <div class="text-center">
-                          <v-icon size="40"> {{ selectedIcon }} </v-icon>
-                        </div>
-                        <!--/Icon-->
-                        <!--Image-->
-                        <validation-provider v-slot="{ errors }" name="Image">
-                          <v-file-input
-                            v-model="timelinePost.image"
-                            label="Image"
-                            filled
-                            prepend-icon="mdi-image"
-                            :error-messages="errors"
-                            class="mt-2"
-                          ></v-file-input>
-                        </validation-provider>
-                        <!--/Image-->
-                      </v-card-text>
-                    </v-window-item>
-                    <v-window-item :value="3">
-                      <v-card-text>
-                        <v-row align="center" justify="center">
-                          <!--/Postpone post-->
-                          <!--New postponed date-->
-                          <v-menu
-                            ref="menu"
-                            v-model="menu"
-                            :close-on-content-click="false"
-                            :return-value.sync="date"
-                            transition="scale-transition"
-                            offset-y
-                            min-width="auto"
-                          >
-                            <template v-slot:activator="{ on, attrs }">
-                              <v-text-field
-                                v-model="date"
-                                label="Post date"
-                                prepend-icon="mdi-calendar"
-                                readonly
-                                v-bind="attrs"
-                                v-on="on"
-                              ></v-text-field>
-                            </template>
-                            <validation-provider
-                              v-slot="{ errors }"
-                              name="select"
-                              rules="required"
-                            >
-                              <v-date-picker
-                                v-model="date"
-                                :min="datePickerFormat"
-                                :error-messages="errors"
-                                color="error"
-                                scrollable
-                                no-title
-                                dark
-                              >
-                                <v-spacer></v-spacer>
-                                <v-btn text color="error" @click="menu = false">
-                                  Cancel
-                                </v-btn>
-                                <v-btn
-                                  text
-                                  color="success"
-                                  @click="$refs.menu.save(date)"
-                                >
-                                  OK
-                                </v-btn>
-                              </v-date-picker>
-                            </validation-provider>
-                          </v-menu>
-
-                          <!---/New postpones date-->
-                        </v-row>
-                        <v-row align="center" justify="center">
-                          <validation-provider
-                            v-slot="{ errors }"
-                            name="hideAuthorCheckbox"
-                          >
-                            <v-checkbox
-                              v-model="hideAuthorCheckbox"
-                              label="Hide author"
-                              :error-messages="errors"
-                            ></v-checkbox>
-                          </validation-provider>
-                        </v-row>
-                        <v-row align="center" justify="center">
-                          <!--Post and Reset buttons-->
-                          <v-btn
-                            color="success"
-                            class="mt-2 mr-4"
-                            type="submit"
-                            :disabled="invalid"
-                          >
-                            post
-                          </v-btn>
-                          <v-btn color="error" class="mt-2 mr-4" @click="reset">
-                            Reset Form
-                          </v-btn>
-                          <!--/Post and Reset buttons-->
-                        </v-row>
-                      </v-card-text>
-                    </v-window-item>
-                  </v-window>
-                </form>
-              </validation-observer>
-              <v-card-actions>
-                <v-btn :disabled="step === 1" text @click="step--">
-                  Back
-                </v-btn>
-                <v-spacer></v-spacer>
-                <v-btn
-                  :disabled="step === 3"
-                  color="error"
-                  depressed
-                  @click="step++"
-                >
-                  Next
-                </v-btn>
-              </v-card-actions>
-            </v-card>
+            <timeline :timelinePost="timelinePost"> </timeline>
           </v-container>
         </v-col>
         <!--All timeline posts-->
-        <v-col align-self="center">
+        <v-col align-self="center" v-if="currentPanelItem == 1">
           <v-card class="mx-auto" max-width="500">
             <v-list two-line>
               <v-list-item-group>
                 <template v-for="(item, i) in reversedNews">
+                  <!--Works, but ESLint gives false error-->
                   <v-list-item :key="item.title">
                     <template>
                       <v-list-item-content>
@@ -368,15 +179,14 @@
                 </template>
               </v-list-item-group>
             </v-list>
-          </v-card>
-        </v-col>
-        <!--/All timeline posts-->
-        <!--***/TIMELINE EDITOR***-->
+          </v-card> </v-col
+        ><!--/All timeline posts-->
       </v-row>
     </v-card>
     <!--Timeline post preview-->
-    <v-row align="center" justify="center" class="mb-6 mt-4">
+    <v-row align="center" justify="center" class="mb-6 mt-2">
       <v-col v-if="currentPanelItem == 1" align-self="center" cols="4">
+        <h1 class="text-center text-3xl">PREVIEW</h1>
         <timelinePost
           :title="
             timelinePost.title != '' ? timelinePost.title : 'title goes here '
@@ -388,9 +198,9 @@
           :date="formattedDate"
         >
         </timelinePost>
-      </v-col>
-    </v-row>
-    <!--/Timeline post preview-->
+      </v-col> </v-row
+    ><!--/Timeline post preview-->
+    <!--***/TIMELINE EDITOR***-->
   </v-container>
 </template>
 <script>
@@ -398,7 +208,6 @@ import "animate.css";
 import store from "@/store";
 import draggable from "vuedraggable";
 import router from "@/router";
-import moment from "moment";
 import { Auth, Admin } from "@/services";
 import { required, max, min } from "vee-validate/dist/rules";
 import rotatingLogo from "@/components/rotatingLogo.vue";
@@ -409,6 +218,7 @@ import {
   setInteractionMode,
 } from "vee-validate";
 
+import timeline from "@/components/admin/timeline.vue";
 import timelinePost from "@/components/timelinePost.vue";
 
 setInteractionMode("eager");
@@ -435,6 +245,7 @@ export default {
     timelinePost,
     rotatingLogo,
     draggable,
+    timeline,
   },
   data: () => ({
     drawer: true,
@@ -472,7 +283,6 @@ export default {
     myCroppa: {},
 
     //Post |Timeline|
-    step: 1,
     timelinePost: {
       title: "",
       text: "",
@@ -480,25 +290,14 @@ export default {
       author: "",
       date: "",
     },
-
     date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
       .toISOString()
       .substr(0, 10),
+    selectedIcon: "mdi-newspaper-variant",
 
-    menu: false,
     hideAuthorCheckbox: null,
     //All timeline posts
     news: Admin.data.getTimelinePosts,
-
-    availableIcons: [
-      { text: "General news", value: "mdi-newspaper-variant" },
-      { text: "Youtube video", value: "mdi-youtube" },
-      { text: "Game news", value: "mdi-controller" },
-      { text: "New Achievement", value: "mdi-trophy" },
-      { text: "Celebration", value: "mdi-party-popper" },
-    ],
-    selectedIcon: "mdi-newspaper-variant",
-    submitting: false,
   }),
   async mounted() {
     await this.getUserDetails();
@@ -533,60 +332,11 @@ export default {
         this.avatarImage = this.avatarImage.data.img;
       }
     },
-    //Timeline post upload//
-    formatDate(unformattedDate) {
-      let day = unformattedDate.slice(-2);
-      let month = unformattedDate.slice(5, 7);
-      let year = unformattedDate.slice(0, 4);
-      let monthsNames = {
-        "01": "January",
-        "02": "February",
-        "03": "March",
-        "04": "April",
-        "05": "May",
-        "06": "June",
-        "07": "July",
-        "08": "August",
-        "09": "September",
-        10: "October",
-        11: "November",
-        12: "December",
-      };
-      let formattedDate = day + " " + monthsNames[month] + ", " + year;
-      return formattedDate;
-    },
-    addNewTimelinePost() {
-      const isValid = this.$refs.observer.validate();
-      console.log(isValid);
-      if (isValid) {
-        this.submitting = true;
-        console.log("Validated successfully!");
-        this.timelinePost.date = this.formatDate(this.date);
-        if (this.hideAuthorCheckbox) this.timelinePost.author = "MacroQuiet";
-
-        this.timelinePost.icon = this.selectedIcon;
-
-        let postData = this.timelinePost;
-
-        Admin.addNewTimelinePost(postData);
-        router.go();
-      }
-    },
     async deleteTimelinePost(post) {
       let response = await Admin.deleteTimelinePost(post);
       if (response.status == 200) {
         router.go();
       }
-    },
-    reset() {
-      Object.keys(this.timelinePost).forEach((key) => {
-        if (key == "image") this.timelinePost[key] = null;
-        else this.timelinePost[key] = "";
-      });
-      this.selectedIcon = "mdi-newspaper-variant";
-      this.hideAuthorCheckbox = null;
-      this.postponedDate = this.formatDate(this.date);
-      this.$refs.observer.reset();
     },
     //Carousel image upload//
     uploadNewCarouselImage() {
@@ -600,15 +350,8 @@ export default {
     },
   },
   computed: {
-    currentTitle() {
-      switch (this.step) {
-        case 1:
-          return "Add new post";
-        case 2:
-          return "Select icon and image";
-        default:
-          return "Pick post date";
-      }
+    handleImages(files) {
+      console.log(files);
     },
     formattedDate() {
       let day = this.date.slice(-2);
@@ -630,17 +373,6 @@ export default {
       };
       let formattedDate = day + " " + monthsNames[month] + ", " + year;
       return formattedDate;
-    },
-    datePickerFormat() {
-      let unformattedDate = moment().toDate();
-      let formattedDate = `${unformattedDate.getFullYear()}-0${
-        unformattedDate.getMonth() + 1
-      }-${unformattedDate.getDate()}`;
-
-      return formattedDate;
-    },
-    handleImages(files) {
-      console.log(files);
     },
   },
 };
